9de74d67c24baa0b692e152b20cc84f4a48c9fd1
Alex Oanea <alex.oanea@mention-me.com>
2020-03-08 23:17:07 +0000

    auto-format

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ class App extends Component {
     persosn: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
-      { name: 'Steph', age: 31 },
+      { name: 'Steph', age: 31 }
     ]
-  }
+  };
   render() {
     return (
       <div className="App">
         <h1>Welcome to React</h1>
         <p>This is really working</p>
         <button>Switch Name</button>
-        <Person name={this.state.persosn[0].name} age={this.state.persosn[0].age}>My Hobbies: Hiking</Person>
-        <Person name={this.state.persosn[1].name} age={this.state.persosn[1].age} />
-        <Person name={this.state.persosn[2].name} age={this.state.persosn[2].age} />
+        <Person
+          name={this.state.persosn[0].name}
+          age={this.state.persosn[0].age}
+        >
+          My Hobbies: Hiking
+        </Person>
+        <Person
+          name={this.state.persosn[1].name}
+          age={this.state.persosn[1].age}
+        />
+        <Person
+          name={this.state.persosn[2].name}
+          age={this.state.persosn[2].age}
+        />
       </div>
     );
   }
