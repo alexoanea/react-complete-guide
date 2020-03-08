99641bfc76798f2536855abdcbfbc503e0fbb369
Alex Oanea <alex.oanea@mention-me.com>
2020-03-08 23:11:13 +0000

    using props and state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persosn: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Steph', age: 31 },
+    ]
+  }
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+        <h1>Welcome to React</h1>
+        <p>This is really working</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persosn[0].name} age={this.state.persosn[0].age}>My Hobbies: Hiking</Person>
+        <Person name={this.state.persosn[1].name} age={this.state.persosn[1].age} />
+        <Person name={this.state.persosn[2].name} age={this.state.persosn[2].age} />
       </div>
     );
   }
