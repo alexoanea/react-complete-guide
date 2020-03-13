2a8d63caa0637249d04f62eb8b972bbe79c9cfbd
Alex Oanea <alex.oanea@mention-me.com>
2020-03-13 02:13:42 +0000

    enabled css modules and switched to using them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -41,6 +42,7 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
@@ -58,25 +60,23 @@ class App extends Component {
           })}
         </div>
       );
+
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Welcome to React</h1>
-        <p className={classes.join(" ")}>This is really working</p>
-        <button
-          className="button"
-          alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}
-        >
+        <p className={assignedClasses.join(" ")}>This is really working</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
